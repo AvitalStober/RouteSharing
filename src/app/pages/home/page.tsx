d4a23e97425c9f60e7e188110aee08c498d51e24
AvitalStober <avitals5050@gmail.com>
2024-11-26 13:33:38 +0200

    delete h1

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -3,7 +3,6 @@ import Map from '@/app/components/GoogleMaps';
 const HomePage = () => {
   return (
     <div style={{ height: '500px', width: '100%' }}>
-      <h1>ברוך הבא למפה שלנו!</h1>
       <Map />
     </div>
   );
